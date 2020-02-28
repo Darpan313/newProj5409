a6e0f06d30e5a6df0ffc9166b1e1f5a4dbd1d342
Darpan Patel <darpanpatel3131@gmail.com>
2020-02-28 14:43:01 -0400

    factorial API created

--- marathonservice/src/main/java/com/service/api/ClaculationRestApi.java
+++ marathonservice/src/main/java/com/service/api/ClaculationRestApi.java
@@ -86,4 +86,71 @@ public class ClaculationRestApi {
         return (a);
     }
 //factorial code goes here
+	    @GET
+    @Path("/factorial/{number}")
+    public Response factorial(@Context UriInfo uriInfo, @PathParam("number") int number) {
+        id++;
+        final String result;
+        if (number < 0) {
+            return Response.status(Response.Status.FORBIDDEN).entity(new GenericAPIResponse() {
+                @Override
+                public int getStatus() {
+                    return Response.Status.FORBIDDEN.getStatusCode();
+                }
+
+                @Override
+                public Object getMessage() {
+                    return "Negative number not allowed";
+                }
+
+            }).build();
+        } else {
+            result = calculateFactorial(number);
+            return Response.ok().entity(new GenericAPIResponse() {
+                @Override
+                public Object getMessage() {
+                    return new HashMap<String, Object>() {
+                        {
+                            put("result", result);
+
+                        }
+                    };
+                }
+                @Override
+                public int getRequestId(){
+                    return id;
+                }
+            }).build();
+        }
+    }
+//Referred GeeksForGeeks: https://www.geeksforgeeks.org/factorial-large-number/
+    private static String calculateFactorial(int a) {
+        String result = "";
+        int res[] = new int[500];
+        res[0] = 1;
+        int res_size = 1;
+
+        for (int i = 2; i <= a; i++) {
+            res_size = multiply(i,res,res_size);
+        }
+        for(int i = res_size-1;i>=0;i--){
+            result+=res[i];
+        }
+        return result;
+    }
+
+    private static int multiply(int x, int res[], int res_size) {
+        int carry = 0;
+        for(int i=0; i<res_size;i++){
+            int prod = res[i]*x + carry;
+            res[i] = prod%10;
+            carry=prod/10;
+        }
+        while (carry!=0){
+            res[res_size]=carry%10;
+            carry=carry/10;
+            res_size++;
+        }
+        return  res_size;
+    }
 }
